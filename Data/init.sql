c36e67806b62e2c1e63b435af806a8208b268835
pietikainen <pietikainen@gmail.com>
2025-02-09 19:33:30 +0200

    add init.sql and seedData.sql

---
+++ Data/init.sql
@@ -0,0 +1,61 @@
+﻿------------------------------------------------------------------------
+-- Kotkangrilli database schema v2
+-- Date: 2025-02-08
+--
+------------------------------------------------------------------------
+-- Start by dropping and deleting all tables, triggers and functions
+------------------------------------------------------------------------
+
+-- Drop all tables if they exist
+DROP TABLE IF EXISTS users, user_level;
+
+-- Delete functions and their triggers if they exist
+DROP FUNCTION IF EXISTS update_updated_at_column CASCADE;
+
+------------------------------------------------------------------------
+-- Begin creating tables, triggers and functions
+------------------------------------------------------------------------
+-- Create function to update updated_at column
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create user level table
+CREATE TABLE user_level (
+                            id SERIAL PRIMARY KEY,
+                            role VARCHAR(50) NOT NULL UNIQUE,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create trigger to update updated_at column in userlevel
+CREATE TRIGGER update_user_level_updated_at
+    BEFORE UPDATE ON user_level
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
+
+-- Create users table
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       snowflake BIGINT NOT NULL UNIQUE,
+                       username VARCHAR(50) NOT NULL,
+                       nickname VARCHAR(50),
+                       avatar VARCHAR(255),
+                       email VARCHAR(50) NOT NULL UNIQUE,
+                       phone VARCHAR(50) UNIQUE,
+                       iban VARCHAR(50) UNIQUE,
+                       user_level_id INT REFERENCES user_level(id) ON DELETE SET NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create trigger to update updated_at column in users
+CREATE TRIGGER update_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
---
+++ Data/seedData.sql
@@ -0,0 +1,23 @@
+﻿-- Kotkangrilli database schema v2
+-- Seed data for database testing
+
+-- Insert seed data for userroles
+INSERT INTO user_level (level) VALUES
+                                   ('user'),
+                                   ('moderator'),
+                                   ('admin')
+    ON CONFLICT (level) DO NOTHING;
+-- Insert seed data for users
+
+INSERT INTO users (snowflake, username, nickname, avatar, email, phone, iban, user_level_id)
+VALUES
+    (12345678, 'matti', 'Masa', 'a1b2c3d4.png', 'matti@example.com', '0401234567', 'FI12 3456 7890 1234', 1),
+    (87654321, 'tiina', 'Tinke', 'e5f6g7h8.png', 'tiina@example.com', '0509876543', 'FI98 7654 3210 9876',2),
+    (23456789, 'erkki', 'Eki', 'i9j0k1l2.png', 'erkki@example.com', '0451122334', 'FI34 5678 9012 3456', 3),
+    (98765432, 'johanna', 'Hanna', 'm3n4o5p6.png', 'johanna@example.com', '0412233445', 'FI56 7890 1234 5678', 1),
+    (34567890, 'pekka', 'Pexi', 'q7r8s9t0.png', 'pekka@example.com', '0445566778', 'FI78 9012 3456 7890', 1)
+    ON CONFLICT (snowflake) DO NOTHING;
+
+
+-- Example update to seed data
+UPDATE users SET nickname = 'Masanaattori' WHERE snowflake = 12345678;
