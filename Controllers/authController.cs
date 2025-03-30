eaafdbb9bca95d6f9643f4cd3c0d70ae2396e348
pietikainen <pietikainen@gmail.com>
2025-03-30 18:32:28 +0300

    commit to update master

---
+++ Controllers/authController.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using AspNet.Security.OAuth.Discord;
+
+namespace kotkangrilli.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("discord")]
+        public IActionResult Login()
+        {
+            var properties = new AuthenticationProperties { RedirectUri = "/" };
+            return Challenge(properties, DiscordAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+
+        [HttpGet("callback")]
+        public async Task<IActionResult> Callback()
+        {
+            var authenticateResult = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            if (!authenticateResult.Succeeded)
+                return BadRequest();
+
+            // Handle successful authentication here
+            return Ok(new { message = "Authentication successful" });
+        }
+    }
+}
